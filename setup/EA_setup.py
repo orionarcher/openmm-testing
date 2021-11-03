e83b25e937defbd226bd77642d19b57cfc5a426c
orioncohen <orion@lbl.gov>
2021-11-03 15:53:05 -0700

    functionality for setting up simulations

---
+++ setup/EA_setup.py
@@ -0,0 +1,121 @@
+from openbabel import pybel
+
+EA = 'CCOC(C)=O'
+PF6 = 'F[P-](F)(F)(F)(F)F'
+TFEA = 'CC(=O)OCC(F)(F)F'
+Li = '[Li+]'
+
+# get the correct force field
+import openff.toolkit as tk
+from openff.toolkit import topology
+from openff.toolkit.typing.engines.smirnoff import ForceField
+import openmm
+
+
+from openbabel import pybel
+from openmm.app import PDBFile
+from openbabel import openbabel
+from pymatgen.io.babel import BabelMolAdaptor
+import parmed as pmd
+import tempfile
+
+from pymatgen.io.lammps.utils import PackmolRunner
+from pymatgen.io.xyz import XYZ
+
+
+def smile_to_mol(smile):
+    mol = pybel.readstring("smi", smile)
+    mol.addh()
+    mol.make3D()
+    adaptor = BabelMolAdaptor(mol.OBMol)
+    return adaptor.pymatgen_mol
+
+
+def smile_to_openmm_system(smile):
+    molecule = tk.topology.Molecule.from_smiles(smile)
+    topology = tk.topology.Topology.from_molecules(molecule)
+    forcefield = tk.typing.engines.smirnoff.ForceField('openff_unconstrained-2.0.0.offxml')
+    system = forcefield.create_openmm_system(topology)
+    return system
+
+
+def smile_to_pbd_file(smile):
+    mol = pybel.readstring("smi", smile)
+    mol.addh()
+    mol.make3D()
+    with tempfile.NamedTemporaryFile() as f:
+        mol.write(format='pdb', filename=f.name, overwrite=True)
+        pdbfile = PDBFile(f.name)
+    return pdbfile
+
+
+def get_coordinates(mols, numbers, box_list):
+    param_list = [{"number": number, "inside box": box_list} for number in numbers]
+    controls = {"maxit": 20, "nloop": 600, 'seed': 20}
+    with tempfile.NamedTemporaryFile() as f:
+        packmol_runner = PackmolRunner(mols, param_list, control_params=controls, auto_box=False, output_file=f.name)
+        packed_box = packmol_runner.run()
+    coordinates = XYZ(packed_box).as_dataframe()
+    return coordinates
+
+
+def make_boxes(x, y=None, z=None):
+    if not (y and z):
+        y, z = x, x
+    packmol_box = [0, 0, 0, x, y, z]
+    openmm_box = [x, y, z, 90, 90, 90]
+    return packmol_box, openmm_box
+
+target_smile = EA
+EA_packbox, EA_openbox = make_boxes(40.8)
+TFEA_packbox, TFEA_openbox = make_boxes(47.8)
+
+
+from openmm.app import *
+from openmm import *
+from openmm.unit import *
+from sys import stdout
+
+
+mol = smile_to_mol(target_smile)
+openmm_system = smile_to_openmm_system(target_smile)
+pbd = smile_to_pbd_file(target_smile)
+EA_sys = pmd.openmm.load_topology(pbd.topology, openmm_system)
+
+coordinates = get_coordinates([mol], [600], EA_packbox)
+coord_values = coordinates.loc[:, 'x':'z'].values
+coord_values -= coord_values.min(axis=0)
+EA_600 = EA_sys * 600
+EA_600.coordinates = coord_values
+EA_600.box = EA_openbox
+EA_system = EA_600.createSystem(nonbondedMethod=PME, nonbondedCutoff=1*nanometer)
+EA_600.save('EA_600.top', overwrite=True)
+
+integrator = LangevinMiddleIntegrator(300*kelvin, 1/picosecond, 0.001*picoseconds)
+EA_simulation = Simulation(EA_600.topology, EA_system, integrator)
+EA_simulation.context.setPositions(coord_values)
+EA_simulation.saveState('../simulations/EA_simulation.xml')
+positions = EA_simulation.context.getState(getPositions=True).getPositions()
+PDBFile.writeFile(EA_simulation.topology, positions, open('../scratch/output.pdb', 'w'))
+
+# target_smile = EA = TFEA
+#
+# mol = smile_to_mol(target_smile)
+# openmm_system = smile_to_openmm_system(target_smile)
+# pbd = smile_to_pbd_file(target_smile)
+# TFEA_sys = pmd.openmm.load_topology(pbd.topology, openmm_system)
+#
+# coordinates = get_coordinates([mol], [600], EA_box)
+# coord_values = coordinates.loc[:, 'x':'z'].values
+# TFEA_600 = TFEA_sys * 600
+# TFEA_600.coordinates = coord_values
+# TFEA_600.box = TFEA_box
+# TFEA_system = TFEA_600.createSystem(nonbondedMethod=PME, nonbondedCutoff=1*nanometer)
+#
+# integrator = LangevinMiddleIntegrator(300*kelvin, 1/picosecond, 0.001*picoseconds)
+# TFEA_simulation = Simulation(EA_600.topology, EA_system, integrator)
+# TFEA_simulation.saveState('../simulations/TFEA_simulation.xml')
+
+
+print('hi')
+
---
+++ setup/setup_functions.py
@@ -0,0 +1,106 @@
+from openbabel import pybel
+import functools
+from pymatgen.io.babel import BabelMolAdaptor
+import parmed
+import tempfile
+from pymatgen.io.lammps.utils import PackmolRunner
+from pymatgen.io.xyz import XYZ
+from openmm.unit import *
+from openmm.app import ForceField, Simulation, PME
+from openmm import LangevinMiddleIntegrator
+from openff.toolkit.typing.engines import smirnoff
+import openff.toolkit as tk
+import openff
+from openff.toolkit import topology
+
+
+def smile_to_structure(smile):
+    mol = pybel.readstring("smi", smile)
+    mol.addh()
+    mol.make3D()
+    with tempfile.NamedTemporaryFile() as f:
+        mol.write(format="pdb", filename=f.name, overwrite=True)
+        structure = parmed.load_file(f.name)
+    return structure
+
+
+def _smiles_to_topology(smiles, counts):
+    assert len(smiles) == len(counts), "smiles and counts must be the same length"
+    structures = [smile_to_structure(smile) for smile in smiles]
+    combined_structs = parmed.Structure()
+    for struct, count in zip(structures, counts):
+        combined_structs += struct * count
+    return combined_structs.topology
+
+
+def smile_to_mol(smile):
+    mol = pybel.readstring("smi", smile)
+    mol.addh()
+    mol.make3D()
+    adaptor = BabelMolAdaptor(mol.OBMol)
+    return adaptor.pymatgen_mol
+
+
+def make_box_list(box_size, box_type):
+    xyz_to_box_funcs = {
+        "packmol": lambda xyz: [0, 0, 0] + xyz,
+        "openmm": lambda xyz: xyz + [90, 90, 90],
+    }
+    assert (
+        box_type in xyz_to_box_funcs.keys()
+    ), f"allowed box types: {' '.join(xyz_to_box_funcs.keys())}"
+    if type(box_size) == int or float:
+        xyz = [box_size, box_size, box_size]
+        return xyz_to_box_funcs[box_type](xyz)
+    elif len(box_size) == 3:
+        xyz = box_size
+        return xyz_to_box_funcs[box_type](xyz)
+    else:
+        TypeError(f"box_size must be a number (cube) or a list (rectangular prism), it is f{box_size}")
+
+
+def _smiles_to_coordinates(smiles, counts, box_size):
+    box_size = make_box_list(box_size, 'packmol')
+    param_list = [{"number": count, "inside box": box_size} for count in counts]
+    controls = {"maxit": 20, "nloop": 600, "seed": 20}
+    mols = [smile_to_mol(smile) for smile in smiles]
+    with tempfile.NamedTemporaryFile() as f:
+        packmol_runner = PackmolRunner(
+            mols,
+            param_list,
+            control_params=controls,
+            auto_box=False,
+            output_file=f.name,
+        )
+        packed_box = packmol_runner.run()
+    coordinates = XYZ(packed_box).as_dataframe()
+    raw_coordinates = coordinates.loc[:, "x":"z"].values
+    return raw_coordinates
+
+from openmm import MonteCarloBarostat
+
+def _smiles_to_simulation(
+    smiles, counts, box_size, integrator=None, **sys_kwargs
+):
+    if integrator is None:
+        integrator = LangevinMiddleIntegrator(
+            300 * kelvin, 1 / picosecond, 0.001 * picoseconds
+        )
+    topology = _smiles_to_topology(smiles, counts)
+    coordinates = _smiles_to_coordinates(smiles, counts, box_size)
+    openff_mols = [
+        openff.toolkit.topology.Molecule.from_smiles(smile) for smile in smiles
+    ]
+    openff_forcefield = smirnoff.ForceField("openff_unconstrained-2.0.0.offxml")
+    openff_topology = openff.toolkit.topology.Topology.from_openmm(
+        topology, openff_mols
+    )
+    system = openff_forcefield.create_openmm_system(openff_topology)
+    structure = parmed.openmm.load_topology(topology, system)
+    structure.box = make_box_list(box_size, 'openmm')
+    system = structure.createSystem(nonbondedMethod=PME, nonbondedCutoff=1 * nanometer)
+    # system.addForce(MonteCarloBarostat(1 * atmosphere, 300 * kelvin, 10))
+    simulation = Simulation(topology, system, integrator)
+    simulation.context.setPositions(coordinates)
+    simulation.context.setPeriodicBoxVectors((4, 0, 0), (0, 4, 0), (0, 0, 4))
+    return simulation
