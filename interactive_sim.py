435e5172cf58b47f371ef605a5ed21b0f328d2cf
orioncohen <orion@lbl.gov>
2021-11-04 13:34:24 -0700

    added file for exploring platform interactively

---
+++ interactive_sim.py
@@ -0,0 +1,26 @@
+from setup.setup_functions import _smiles_to_simulation
+
+from openmm.unit import *
+
+from openmm.app import PDBReporter
+from openmm.app import StateDataReporter
+from sys import stdout
+from openmm import MonteCarloBarostat, Platform
+
+from openmm import *
+from openmm.app import *
+
+EA = "CCOC(C)=O"
+PF6 = "F[P-](F)(F)(F)(F)F"
+TFEA = "CC(=O)OCC(F)(F)F"
+Li = "[Li+]"
+
+sim = _smiles_to_simulation(
+    [EA],
+    [600],
+    80,
+)
+
+context = sim.context
+platform = Platform.getPlatformByName("CUDA")
+print(platform.getPropertyValue(context, "DeviceIndex"))
