e99e5e4298380d09489e5f47658193a84e370389
orioncohen <orion@lbl.gov>
2021-11-16 09:59:03 -0800

    add production sim for testing on HPC resources

--- production_sim.py
+++ production_sim.py
@@ -11,6 +11,7 @@ import time
 
 EA = "CCOC(C)=O"
 PF6 = "F[P-](F)(F)(F)(F)F"
+FEC = "C1C(OC(=O)O1)F"
 TFEA = "CC(=O)OCC(F)(F)F"
 Li = "[Li+]"
 
@@ -22,7 +23,7 @@ rank = comm.Get_rank()
 
 print("rank: ", rank)
 
-properties = {"DeviceIndex": f"{rank}"}
+properties = {"DeviceIndex": f"{1}"}
 
 scaling_dict = {0: 0.7, 1: 0.8, 2: 0.9, 3: 1.0}
 charge_scaling = scaling_dict[rank]
@@ -30,21 +31,27 @@ charge_scaling = scaling_dict[rank]
 print("charge scaling: ", charge_scaling)
 
 sim = _smiles_to_simulation(
-    [EA, TFEA, Li, PF6],
-    [600, 100, 50, 50],
+    [EA, TFEA, FEC, Li, PF6],
+    [500, 100, 50, 50, 50],
     50,
     charge_scaling=0.7,
     properties=properties,
 )
-
-sim.reporters.append(StateDataReporter(stdout, 100, step=True, potentialEnergy=True, temperature=True, volume=True, density=True))
 
 
 context = sim.context
 platform = context.getPlatform()
 system = context.getSystem()
 integrator = context.getIntegrator()
+positions = context.getState(getPositions=True).getPositions(asNumpy=True)
 
+sim.reporters.append(StateDataReporter("output/state.txt", 1000, step=True, potentialEnergy=True, temperature=True, volume=True, density=True))
+sim.reporters.append(DCDReporter("output/test.dcd", 1000))
+
+pdb_reporter = PDBReporter("output/test.pdb", 1000)
+pdb_reporter.report(sim, context.getState(getPositions=True))
+
+PDBFile("output/test2.pdb")
 
 start = time.time()
 
@@ -61,15 +68,15 @@ sim.context.reinitialize(preserveState=True)
 # annealing
 for i in np.arange(0, 100, 1):
     integrator.setTemperature(300 * kelvin + 1 * i * kelvin)
-    sim.step(100)
+    sim.step(10)
 
 sim.step(1000)
 
 for i in np.arange(100, 0, -1):
     integrator.setTemperature(300 * kelvin + 1 * i * kelvin)
-    sim.step(100)
+    sim.step(10)
 
-sim.step(1000)
+sim.step(100)
 
 end = time.time()
 print("time: ", end - start)
