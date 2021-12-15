e298674bf14677f079b867da88ed09cc20eb09ba
orioncohen <orion@lbl.gov>
2021-12-15 15:01:51 -0800

    debugging remotely

--- pymatgen_sims/pymatgen_loading_test.py
+++ pymatgen_sims/pymatgen_loading_test.py
@@ -7,11 +7,11 @@ sim2 = OpenMMSet.from_directory('bad_input_set')
 opencl = Platform.getPlatformByName("OpenCL")
 cpu = Platform.getPlatformByName("CPU")
 
-sim1.get_simulation()
-sim2.get_simulation()
-
-sim1.get_simulation(cpu)
-sim2.get_simulation(cpu)
-
-sim1.get_simulation(opencl)
+# sim1.get_simulation()
+# sim2.get_simulation()
+#
+# sim1.get_simulation(cpu)
+# sim2.get_simulation(cpu)
+#
+# sim1.get_simulation(opencl)
 sim2.get_simulation(opencl)
