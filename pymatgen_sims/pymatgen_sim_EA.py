155485c6d7c3252b0db3ea9b98547c7ed7ae80e8
orioncohen <orion@lbl.gov>
2021-12-14 16:35:45 -0800

    comment everything

--- pymatgen_sims/pymatgen_sim_EA.py
+++ pymatgen_sims/pymatgen_sim_EA.py
@@ -39,75 +39,75 @@ sim = _smiles_to_simulation(
 )
 
 print("made first sim")
-
-temperature_dict = {0: 298, 1: 273, 2: 253, 3: 233}
-
-temperature = temperature_dict[int(rank)]
-
-generator = OpenMMSolutionGen(
-    partial_charge_scaling={Li: 0.8, PF6: 0.8},
-    partial_charges=[(pf6, pf6_charges), (li, li_charges)],
-    temperature=temperature
-)
-
-input_set = generator.get_input_set(
-    {EA: 522, FEC: 78, Li: 54, PF6: 54},
-    density=1.06
-)
-
-run_names = {  # change
-    0: "EA_298",
-    1: "EA_273",
-    2: "EA_253",
-    3: "EA_233",
-}
-
-
-# platform = Platform.getPlatformByName("OpenCL")
-properties = {"DeviceIndex": f"{rank}"}  # change
-platform = Platform.getPlatformByName("OpenCL")
-
-
-system = input_set.inputs['system.xml'].get_system()
-topology = input_set.inputs['topology.pdb'].get_topology()
-integrator = input_set.inputs['integrator.xml'].get_integrator()
-state = input_set.inputs['state.xml'].get_state()
-
-sim = openmm.app.Simulation(
-    topology,
-    system,
-    integrator,
-    platform=platform,
-    platformProperties=properties
-)
-
-sim.context.setState(state)
-
-# sim = input_set.get_simulation(
-#     platform=platform,
-#     platformProperties=properties,
+#
+# temperature_dict = {0: 298, 1: 273, 2: 253, 3: 233}
+#
+# temperature = temperature_dict[int(rank)]
+#
+# generator = OpenMMSolutionGen(
+#     partial_charge_scaling={Li: 0.8, PF6: 0.8},
+#     partial_charges=[(pf6, pf6_charges), (li, li_charges)],
+#     temperature=temperature
 # )
-
-dir = "ea_runs"  # change
-name = run_names[rank]
-os.makedirs(f"{dir}/{name}", exist_ok=True)
-sim.reporters.append(
-    StateDataReporter(f"{dir}/{name}/state.txt", 1000, step=True, potentialEnergy=True, temperature=True,
-                      volume=True, density=True))
-sim.reporters.append(DCDReporter(f"{dir}/{name}/trajectory.dcd", 1000))
-
-pdb_reporter = PDBReporter(f"{dir}/{name}/topology.pdb", 1)
-pdb_reporter.report(sim, sim.context.getState(getPositions=True))
-sim.minimizeEnergy()
-start = time.time()
-equilibrate_pressure(sim, 1000)
-anneal(sim, 400, [1000, 1000, 1000])
-mid = time.time()
-sim.step(2000)
-
-platform = sim.context.getPlatform()
-print("rank: ", rank)
-print("platform: ", platform.getName())
-print("mid time: ", mid - start)
-print("end time: ", time.time() - start)
-print("device index: ", platform.getPropertyValue(sim.context, "DeviceIndex"))
+#
+# input_set = generator.get_input_set(
+#     {EA: 522, FEC: 78, Li: 54, PF6: 54},
+#     density=1.06
+# )
+#
+# run_names = {  # change
+#     0: "EA_298",
+#     1: "EA_273",
+#     2: "EA_253",
+#     3: "EA_233",
+# }
+#
+#
+# # platform = Platform.getPlatformByName("OpenCL")
+# properties = {"DeviceIndex": f"{rank}"}  # change
+# platform = Platform.getPlatformByName("OpenCL")
+#
+#
+# system = input_set.inputs['system.xml'].get_system()
+# topology = input_set.inputs['topology.pdb'].get_topology()
+# integrator = input_set.inputs['integrator.xml'].get_integrator()
+# state = input_set.inputs['state.xml'].get_state()
+#
+# sim = openmm.app.Simulation(
+#     topology,
+#     system,
+#     integrator,
+#     platform=platform,
+#     platformProperties=properties
+# )
+#
+# sim.context.setState(state)
+#
+# # sim = input_set.get_simulation(
+# #     platform=platform,
+# #     platformProperties=properties,
+# # )
+#
+# dir = "ea_runs"  # change
+# name = run_names[rank]
+# os.makedirs(f"{dir}/{name}", exist_ok=True)
+# sim.reporters.append(
+#     StateDataReporter(f"{dir}/{name}/state.txt", 1000, step=True, potentialEnergy=True, temperature=True,
+#                       volume=True, density=True))
+# sim.reporters.append(DCDReporter(f"{dir}/{name}/trajectory.dcd", 1000))
+#
+# pdb_reporter = PDBReporter(f"{dir}/{name}/topology.pdb", 1)
+# pdb_reporter.report(sim, sim.context.getState(getPositions=True))
+# sim.minimizeEnergy()
+# start = time.time()
+# equilibrate_pressure(sim, 1000)
+# anneal(sim, 400, [1000, 1000, 1000])
+# mid = time.time()
+# sim.step(2000)
+#
+# platform = sim.context.getPlatform()
+# print("rank: ", rank)
+# print("platform: ", platform.getName())
+# print("mid time: ", mid - start)
+# print("end time: ", time.time() - start)
+# print("device index: ", platform.getPropertyValue(sim.context, "DeviceIndex"))
