0745691a64c543e4839e273e2d6e1ee9fced4421
orioncohen <orion@lbl.gov>
2021-12-14 16:43:08 -0800

    add minimal run

--- pymatgen_sims/pymatgen_sim_EA.py
+++ pymatgen_sims/pymatgen_sim_EA.py
@@ -1,17 +1,17 @@
 from mpi4py import MPI
 
-# from pymatgen.io.openmm.generators import OpenMMSolutionGen
+from pymatgen.io.openmm.generators import OpenMMSolutionGen
 # from pymatgen.io.openmm.simulations import equilibrate_pressure, anneal
-# from pymatgen.core import Molecule
+from pymatgen.core import Molecule
 #
-# import numpy as np
+import numpy as np
 #
 # import time
 # import os
 #
 # from openmm.app import StateDataReporter, PDBReporter, DCDReporter
 # import openmm
-# from openmm import Platform
+from openmm import Platform
 from old_setup_functions import _smiles_to_simulation
 
 
@@ -21,22 +21,22 @@ TFEA = "CC(=O)OCC(F)(F)F"
 FEC = "O=C1OC[C@H](F)O1"
 Li = "[Li+]"
 
-# pf6_charges = np.load('../partial_charges/PF6.npy')
-# pf6 = Molecule.from_file('../partial_charges/PF6.xyz')
-# li_charges = np.load('../partial_charges/Li.npy')
-# li = Molecule.from_file('../partial_charges/Li.xyz')
+pf6_charges = np.load('../partial_charges/PF6.npy')
+pf6 = Molecule.from_file('../partial_charges/PF6.xyz')
+li_charges = np.load('../partial_charges/Li.npy')
+li = Molecule.from_file('../partial_charges/Li.xyz')
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-sim = _smiles_to_simulation(
-    [TFEA, FEC, Li, PF6],
-    [512, 88, 62, 62],
-    47.8,
-    charge_scaling=0.7,
-    properties={"DeviceIndex": f"{1}"},
-    temperature=298
-)
+# sim = _smiles_to_simulation(
+#     [TFEA, FEC, Li, PF6],
+#     [512, 88, 62, 62],
+#     47.8,
+#     charge_scaling=0.7,
+#     properties={"DeviceIndex": f"{1}"},
+#     temperature=298
+# )
 
 print("made first sim")
 #
@@ -44,16 +44,16 @@ print("made first sim")
 #
 # temperature = temperature_dict[int(rank)]
 #
-# generator = OpenMMSolutionGen(
-#     partial_charge_scaling={Li: 0.8, PF6: 0.8},
-#     partial_charges=[(pf6, pf6_charges), (li, li_charges)],
-#     temperature=temperature
-# )
+generator = OpenMMSolutionGen(
+    partial_charge_scaling={Li: 0.8, PF6: 0.8},
+    partial_charges=[(pf6, pf6_charges), (li, li_charges)],
+    temperature=298
+)
 #
-# input_set = generator.get_input_set(
-#     {EA: 522, FEC: 78, Li: 54, PF6: 54},
-#     density=1.06
-# )
+input_set = generator.get_input_set(
+    {EA: 522, FEC: 78, Li: 54, PF6: 54},
+    density=1.06
+)
 #
 # run_names = {  # change
 #     0: "EA_298",
@@ -64,8 +64,8 @@ print("made first sim")
 #
 #
 # # platform = Platform.getPlatformByName("OpenCL")
-# properties = {"DeviceIndex": f"{rank}"}  # change
-# platform = Platform.getPlatformByName("OpenCL")
+properties = {"DeviceIndex": f"{rank}"}  # change
+platform = Platform.getPlatformByName("OpenCL")
 #
 #
 # system = input_set.inputs['system.xml'].get_system()
@@ -83,10 +83,10 @@ print("made first sim")
 #
 # sim.context.setState(state)
 #
-# # sim = input_set.get_simulation(
-# #     platform=platform,
-# #     platformProperties=properties,
-# # )
+sim = input_set.get_simulation(
+    platform=platform,
+    platformProperties=properties,
+)
 #
 # dir = "ea_runs"  # change
 # name = run_names[rank]
