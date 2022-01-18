c5f6b507de958263d3579c641b53e0a801ad141f
orioncohen <orion@lbl.gov>
2022-01-17 20:27:40 -0800

    edited submit.py

--- simulations/submission_scripts/submit.py
+++ simulations/submission_scripts/submit.py
@@ -37,7 +37,6 @@ output_dir = output_dir / target_path.stem
 output_dir.mkdir(parents=True, exist_ok=True)
 pdb_reporter = PDBReporter(str(output_dir / "topology.pdb"), 1)
 pdb_reporter.report(sim, sim.context.getState(getPositions=True))
-sim.reporters.append(DCDReporter(str(output_dir / "trajectory.dcd"), 1000))
 sim.reporters.append(
     StateDataReporter(
         str(output_dir / "state.txt"),
@@ -49,12 +48,16 @@ sim.reporters.append(
         density=True,
     )
 )
+sim.reporters.append(DCDReporter(str(output_dir / "trajectory_equil.dcd"), 10000))
 
 start = time.time()
 
 sim.minimizeEnergy()
 equilibrate_pressure(sim, 1000000)
 anneal(sim, 400, [1000000, 1000000, 1000000])
+sim.reporters.pop()
+sim.reporters.append(DCDReporter(str(output_dir / "trajectory.dcd"), 10000))
+
 sim.step(5000000)
 
 print("total runtime: ", time.time() - start)
